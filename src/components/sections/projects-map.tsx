f303f12c9bed2e912158d6f667af37080315ac84
Kyle Caprio <capriok7@yahoo.com>
2020-12-13 17:02:12 -0700

    collection bug

--- src/components/sections/projects-map.tsx
+++ src/components/sections/projects-map.tsx
@@ -45,11 +45,13 @@ const ProjectsMap = () => {
       && p.title !== feat[2].node.title
   })
 
+  console.log(filteredFeatures)
+
   return (
     <div className="projects-map-section">
       <h1 className="section-title">Full Collection</h1>
       <div className="cards-cont">
-        {data.allProjectsJson.edges.slice(0, atHome ? pg : data.allProjectsJson.edges.length).map(({ node: project }, i) => (
+        {filteredFeatures.slice(0, atHome ? pg : filteredFeatures.length).map(({ node: project }, i) => (
           <ProjectCard key={i} project={project} />
         ))}
       </div>
@@ -61,7 +63,7 @@ const ProjectsMap = () => {
         </button>
         <button
           onClick={() => setPg(pg + 3)}
-          disabled={pg >= data.allProjectsJson.edges.length}>
+          disabled={pg >= filteredFeatures.length}>
           <MdExpandMore />
         </button>
       </div>}
